a0618e30b7a8fa73e732dc6e98fd795e3c6998b9
Atharva Panchal <patharvar4166@gmail.com>
2025-02-05 16:19:45 +0530

    commit

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -1,29 +1,35 @@
-import { image } from "@heroui/theme";
+
 import Image from "next/image";
 
 
 const imageStyle = {
     borderRadius: '50%',
     border: '1px',
-  }
+}
+
+const colorName = {
+    color:'#795757'
+}
+const colorIntro = {
+    color:'#3B3030'
+}
 
 export default function Hero(){
     return(
-        <section className="backdrop-blur-lg">
-            <div className="grid grid-cols-2 p-10 content-center justify-items-center " style={{ height: 'calc(100vh - 5rem)' }}>
-                <div className="">
-                    <h1 className="text-4xl font-bold">I am <span className="">Atharva</span> a Software Developer From India</h1>
-                </div>
-                <div className="">
-                    <Image
-                        src="/assets/Profile-img.png"
-                        width={300}
-                        height={300}
-                        alt="profile"
-                        style={imageStyle}
-                    />
-                </div>
+        <section className="grid gap-4 justify-items-center">
+            <div className="p-6 content-center">
+                <Image
+                    src="/assets/Profile-img.png"
+                    width={300}
+                    height={300}
+                    alt="profile"
+                    style={imageStyle}
+                />
             </div>
+            <div className="p-5 content-center justify-items-center text-center">
+                <h1 className="text-4xl font-semibold" style={colorIntro}>I am <span className=" text-5xl font-bold" style={colorName}>Atharva</span> A Software Developer From India</h1>
+            </div>
+                
         </section>
     );
 }
